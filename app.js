1554af191a2eee0d49778da24371417bf1aaf9ee
Anilthapa4 <107929147+Anilthapa4@users.noreply.github.com>
2023-01-31 23:45:41 +0545

    bugs

--- app.js
+++ app.js
@@ -9,33 +9,40 @@ app.get("/", function (req, res) {
     const today = new Date();
     var day = "";
 
-    switch (today.getDay()) {
-        case 0:
-            day = "Sunday"
-            break;
-        case 1:
-            day = "Monday"
-            break;
-        case 2:
-            day = "Tuesday"
-            break;
-        case 3:
-            day = "Thursday"
-            break;
-        case 4:
-            day = "Wednesday"
-            break;
-        case 5:
-            day = "Friday"
-            break;
-        case 6:
-            day = "Saturday"
-            break;
+    // switch (today.getDay()) {
+    //     case 0:
+    //         day = "Sunday"
+    //         break;
+    //     case 1:
+    //         day = "Monday"
+    //         break;
+    //     case 2:
+    //         day = "Tuesday"
+    //         break;
+    //     case 3:
+    //         day = "Thursday"
+    //         break;
+    //     case 4:
+    //         day = "Wednesday"
+    //         break;
+    //     case 5:
+    //         day = "Friday"
+    //         break;
+    //     case 6:
+    //         day = "Saturday"
+    //         break;
 
-        default:
-            console.log("Error");
+    //     default:
+    //         console.log("Error");
+    // }
+    // res.render("list",{kindOfDay : day});
+    if(today.getDay()===3){
+        day="tuesday";
+        res.render("list",{kindOfDay: day});
+    } else {
+        day="wednesday";
+        res.render("list",{kindOfDay: day});
     }
-    res.render("list",{kindOfDay : day});
 
 });
 
