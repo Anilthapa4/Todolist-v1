495be87c7689c95a2c39176c685faf2ee84f9778
Anilthapa4 <107929147+Anilthapa4@users.noreply.github.com>
2023-02-01 01:14:13 +0545

    passing data from webpage to server

--- app.js
+++ app.js
@@ -3,48 +3,36 @@ import bodyParser from "body-parser";
 
 const app = express();
 
+var items=["Eat","Code","Sleep & Repeat"];
+
 app.set('view engine', 'ejs');
+//ejs template
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 app.get("/", function (req, res) {
     const today = new Date();
-    var day = "";
 
-    // switch (today.getDay()) {
-    //     case 0:
-    //         day = "Sunday"
-    //         break;
-    //     case 1:
-    //         day = "Monday"
-    //         break;
-    //     case 2:
-    //         day = "Tuesday"
-    //         break;
-    //     case 3:
-    //         day = "Thursday"
-    //         break;
-    //     case 4:
-    //         day = "Wednesday"
-    //         break;
-    //     case 5:
-    //         day = "Friday"
-    //         break;
-    //     case 6:
-    //         day = "Saturday"
-    //         break;
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
 
-    //     default:
-    //         console.log("Error");
-    // }
-    // res.render("list",{kindOfDay : day});
-    if(today.getDay()===3){
-        day="tuesday";
-        res.render("list",{kindOfDay: day});
-    } else {
-        day="wednesday";
-        res.render("list",{kindOfDay: day});
-    }
+    var day = today.toLocaleDateString("en-US", options);
 
+    res.render("list", { kindOfDay: day, newListItems:items});
 });
+
+app.post("/", function (req, res) {
+   var item=req.body.newitem;
+   items.push(item);
+   //this pushes the new added item to the array called "items".
+   res.redirect("/");
+   //this redirect to our home route.
+});
+
 
 app.listen(3000, function () {
     console.log("Server is running on port 3000");
